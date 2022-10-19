7141cbb921820c6c89dcff0410eb37a47f67a937
xentoshi <xentoshi@protonmail.com>
2022-10-19 23:07:00 +0200

    Update index.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Box, Center, Spacer, Stack } from '@chakra-ui/react'
 import NavBar from '../components/NavBar'
